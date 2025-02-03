2d4c746668b26ecdaa001ce5546aa878c901f566
Ritesh Kumar <ritesh.kumar@in.pega.com>
2025-02-03 14:22:13 +0530

    Updated nifty OI Script

--- python-scripts/Crypto/show-portfolio.py
+++ python-scripts/Crypto/show-portfolio.py
@@ -3,6 +3,7 @@ import json
 import sys
 from tabulate import tabulate
 from decimal import * 
+from datetime import datetime
 
 sys.set_int_max_str_digits(10002)
 
@@ -16,18 +17,51 @@ def strCyan(skk):        return "\033[96m {}\033[00m".format(skk)
 def strLightGray(skk):   return "\033[97m {}\033[00m".format(skk)
 def strBlack(skk):       return "\033[98m {}\033[00m".format(skk)
 def strBold(skk):        return "\033[1m {}\033[0m".format(skk)
+def printhr():           print(strYellow("|".rjust(150,"-")))
+
+def print_header(portfolioname="",datetime=0,invested=0,curvalue=0,profitloss=0,profitlossper=0):
+    portfolio_name = strPurple(portfolioname.rjust(75))
+    print(portfolio_name)
+    printhr()
+    
+    if(Decimal(profitloss) > 0):
+        profitloss = strGreen(profitloss)
+        profitlossper = strGreen(profitlossper)
+    else:
+        profitloss = strRed(profitloss)
+        profitlossper = strRed(profitlossper)
+        
+    header_string = strLightPurple(" Date Time:") + strBold(datetime) \
+                    + strLightPurple(" Invested:") + strBold(invested + " \u20B9") \
+                    + strLightPurple(" Current Value:") + strBold(curvalue + " \u20B9") \
+                    + strLightPurple(" Profit/Loss:") + strBold(profitloss + " \u20B9") \
+                    + strLightPurple(" Profit/Loss %:") + strBold(profitlossper)
+    print(header_string)
+
+def color_code(value):
+    if int(value) < 1:
+        return strRed(value)
+    elif int(value) > 1:
+        return strGreen(value)
+    else:
+        return value
 
   
 if __name__ == '__main__': 
+    
     portfolio_data = coinswitchapi.getPortfolio()
     portfolio_json = json.loads(portfolio_data.text)
-    # portfolio_json = sorted(portfolio_json, key=lambda k: k['data']['invested_value'], reverse=True)
     
+    invested = 0.0
+    curvalue = 0.0 
+    profitloss = 0.0
+    profitlossper = 0.0  
     data_array = []
     data_array_header = ['NAME','CODE','UNITS','BUY-AVG-PRICE \u20B9','INVESTMENT-VALUE \u20B9','CURR-PRICE \u20B9','CURRENT-VALUE \u20B9','PROFIT-LOSS \u20B9','PROFIT-LOSS-%']
     for item in portfolio_json['data']: 
         profit_loss = Decimal(item["current_value"]) - Decimal(item["invested_value"]) 
         profit_loss_per = 100 - (Decimal(item["current_value"]) * 100)/Decimal(item["invested_value"])
+        
         if(profit_loss > 0):
             profit_loss = strGreen(profit_loss)
             profit_loss_per = strGreen(profit_loss_per)
@@ -35,17 +69,39 @@ if __name__ == '__main__':
             profit_loss = strRed(profit_loss)
             profit_loss_per = strRed(profit_loss_per)
         
-        row_data = [item["name"],
-                    item["currency"],
-                    item["main_balance"],
-                    Decimal(item["buy_average_price"]),
-                    Decimal(item["invested_value"]),
-                    item["sell_rate"],
-                    Decimal(item["current_value"]),
-                    profit_loss,
-                    profit_loss_per                  
-                ]
+        if(item["currency"] == "INR"):
+            invested = Decimal(item["invested_value"])
+            curvalue = Decimal(item["current_value"])
+            profitloss = curvalue - invested
+            profitlossper = 100 - (curvalue*100)/invested
+            
+            invested = f'{invested:.2f}'
+            curvalue = f'{curvalue:.2f}'
+            profitloss = f'{profitloss:.2f}'
+            profitlossper = f'{profitlossper:.2f}'
+        else:
+            row_data = [item["name"],
+                        item["currency"],
+                        item["main_balance"],
+                        Decimal(item["buy_average_price"]),
+                        Decimal(item["invested_value"]),
+                        item["sell_rate"],
+                        Decimal(item["current_value"]),
+                        profit_loss,
+                        profit_loss_per                  
+                    ]
                 
-        data_array.append(row_data)
+            data_array.append(row_data)
+            data_array = sorted(data_array, key=lambda name: name[1])
+    
 
-    print(tabulate(data_array, headers=data_array_header, floatfmt=".2f"))
+    # Printing Data On Screen
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    
+    print('\033c')
+    printhr()
+    print_header("CoinSwitch Portfolio", dt_string, invested, curvalue, profitloss, profitlossper)
+    printhr()
+    print(tabulate(data_array, headers=data_array_header, floatfmt=".6f", tablefmt="simple"))
+    printhr()
---
+++ python-scripts/NSE/oi-banknifty.py
@@ -0,0 +1,222 @@
+import aiohttp
+import asyncio
+import requests
+import json
+import math
+import time
+from tabulate import tabulate
+from datetime import datetime
+
+def strRed(skk):         return "\033[91m {}\033[00m".format(skk)
+def strGreen(skk):       return "\033[92m {}\033[00m".format(skk)
+def strYellow(skk):      return "\033[93m {}\033[00m".format(skk)
+def strLightPurple(skk): return "\033[94m {}\033[00m".format(skk)
+def strPurple(skk):      return "\033[95m {}\033[00m".format(skk)
+def strCyan(skk):        return "\033[96m {}\033[00m".format(skk)
+def strLightGray(skk):   return "\033[97m {}\033[00m".format(skk)
+def strBlack(skk):       return "\033[98m {}\033[00m".format(skk)
+def strBold(skk):        return "\033[1m {}\033[00m".format(skk)
+
+def round_nearest(x, num=50): return int(math.ceil(float(x)/num)*num)
+def nearest_strike_bnf(x): return round_nearest(x, 100)
+def nearest_strike_nf(x): return round_nearest(x, 50)
+
+url_oc      = "https://www.nseindia.com/option-chain"
+url_bnf     = 'https://www.nseindia.com/api/option-chain-indices?symbol=BANKNIFTY'
+url_nf      = 'https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY'
+url_indices = "https://www.nseindia.com/api/allIndices"
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+    'accept-language': 'en,gu;q=0.9,hi;q=0.8',
+    'accept-encoding': 'gzip, deflate, br'
+}
+
+cookies = dict()
+
+# Showing Header in structured format with Last Price and Nearest Strike
+def print_header(index="",ul=0,nearest=0,expiry_date=None):
+    header_string = strPurple( index.ljust(12," ") + " => ") \
+                    + strLightPurple(" Expiry Date: ") + strBold(expiry_date) \
+                    + strLightPurple(" Last Price: ") + strBold(str(ul)) \
+                    + strLightPurple(" Nearest Strike: ") + strBold(str(nearest))
+    print(header_string)
+
+def print_hr():
+    print(strYellow("|".rjust(150,"-")))
+
+def color_code(value):
+    if int(value) < 1:
+        return strRed(value)
+    elif int(value) > 1:
+        return strGreen(value)
+    else:
+        return value
+
+def analysePCR(pcr):    
+    returnMSG = None
+    if (pcr > 4):
+        returnMSG = strGreen("Extreamly Bullish")
+    elif (4 > pcr > 1):
+        returnMSG = strGreen("Bullish")
+    elif (pcr == 1):
+        returnMSG = strYellow("Sideways")
+    elif (1 > pcr > 0.4):
+        returnMSG = strRed("Bearish")
+    elif (0.4 > pcr):
+        returnMSG = strRed("Extreamly Bearish")
+    
+    return returnMSG  
+
+def set_cookie():
+    sess = requests.Session()
+    request = sess.get(url_oc, headers=headers, timeout=5)
+    return dict(request.cookies)
+
+async def get_data(url, session):
+    global cookies
+    async with session.get(url, headers=headers, timeout=5, cookies=cookies) as response:
+        if response.status == 401:
+            cookies = set_cookie()
+            async with session.get(url, headers=headers, timeout=5, cookies=cookies) as response:
+                return await response.text()
+        elif response.status == 200:
+            return await response.text()
+        return ""
+
+async def fetch_all_data():
+    async with aiohttp.ClientSession() as session:
+        indices_data = await get_data(url_indices, session)
+        bnf_data = await get_data(url_bnf, session)
+        nf_data = await get_data(url_nf, session)
+    return indices_data, bnf_data, nf_data
+
+# Process the fetched data
+def process_indices_data(data):
+    global bnf_ul, nf_ul, bnf_nearest, nf_nearest
+    data = json.loads(data)    
+    for index in data["data"]:
+        if index["index"] == "NIFTY 50":
+            nf_ul = index["last"]
+        if index["index"] == "NIFTY BANK":
+            bnf_ul = index["last"]
+    bnf_nearest = nearest_strike_bnf(bnf_ul)
+    nf_nearest = nearest_strike_nf(nf_ul)    
+
+def process_oi_data(data, nearest, step, num):
+    global expiryDate
+    data = json.loads(data)
+    currExpiryDate = data["records"]["expiryDates"][0]  
+    expiryDate = data["records"]["expiryDates"][0]  
+    oi_data = []
+    for item in data['records']['data']:
+        if item["expiryDate"] == currExpiryDate:
+            if nearest - step*num <= item["strikePrice"] <= nearest + step*num:
+                oi_data.append((
+                    item["strikePrice"], 
+                    item["CE"]["openInterest"], 
+                    item["PE"]["openInterest"], 
+                    item["CE"]["impliedVolatility"], 
+                    item["PE"]["impliedVolatility"], 
+                    item["CE"]["lastPrice"],
+                    item["PE"]["lastPrice"],
+                    item["CE"]["changeinOpenInterest"],
+                    item["PE"]["changeinOpenInterest"]
+                ))
+    return oi_data
+
+def print_oi_data(index_name, index_data, previous_data=None):    
+    # set_header(url_indices)
+    print('\033c')
+    print_hr()
+    print_header(index_name, nf_ul, nf_nearest, expiryDate)
+    print_hr()
+    
+    # print(nifty_data)
+    
+    data_array = []
+    data_array_header = ['CE-OI','CE-CHANGE-OI','CE-IV','CE-LTP','PCR-OI','STRIKE-PRICE','PCR-COI','PE-LTP','PE-IV','PE-CHANGE-OI','PE-OI','CONCLUSION']
+    
+    for i, (strike, ce_oi, pe_oi, ce_iv, pe_iv, ce_lp, pe_lp, ce_cioi, pe_cioi) in enumerate(index_data):
+        pcrvalue_coi = 0 
+        if(int(ce_cioi) == 0):
+            pcrvalue_coi = 0
+        else:
+            pcrvalue_coi = round(pe_cioi/ce_cioi,2)
+            
+        pcrvalue_oi = 0
+        if(int(ce_oi) == 0):
+            pcrvalue_oi = 0
+        else:
+            pcrvalue_oi = round(pe_oi/ce_oi,2)  
+        
+        row_data = [
+                ce_oi,
+                color_code(ce_cioi),
+                ce_iv,
+                ce_lp,
+                str(pcrvalue_oi),
+                strike,
+                str(pcrvalue_coi),
+                pe_lp,
+                pe_iv,
+                color_code(pe_cioi),
+                pe_oi,
+                analysePCR(pcrvalue_coi)
+            ]
+        
+        data_array.append(row_data)
+
+    print(tabulate(data_array, headers=data_array_header))      
+        
+        
+    # print(strBold(strLightPurple("Nifty Open Interest:")))
+    # for i, (strike, ce_oi, pe_oi) in enumerate(nifty_data):
+    #     ce_change = ce_oi - prev_nifty_data[i][1] if prev_nifty_data else 0
+    #     pe_change = pe_oi - prev_nifty_data[i][2] if prev_nifty_data else 0
+    #     ce_color = strGreen(ce_oi) if ce_change > 0 else strRed(ce_oi)
+    #     pe_color = strGreen(pe_oi) if pe_change > 0 else strRed(pe_oi)
+    #     print(f"Strike Price: {strike}, Call OI: {ce_color} ({strBold(f'+{ce_change}') if ce_change > 0 else strBold(ce_change) if ce_change < 0 else ce_change}), Put OI: {pe_color} ({strBold(f'+{pe_change}') if pe_change > 0 else strBold(pe_change) if pe_change < 0 else pe_change})")
+
+    # print(strBold(strLightPurple("\nBank Nifty Open Interest:")))
+    # for i, (strike, ce_oi, pe_oi) in enumerate(bank_nifty_data):
+    #     ce_change = ce_oi - prev_bank_nifty_data[i][1] if prev_bank_nifty_data else 0
+    #     pe_change = pe_oi - prev_bank_nifty_data[i][2] if prev_bank_nifty_data else 0
+    #     ce_color = strGreen(ce_oi) if ce_change > 0 else strRed(ce_oi)
+    #     pe_color = strGreen(pe_oi) if pe_change > 0 else strRed(pe_oi)
+    #     print(f"Strike Price: {strike}, Call OI: {ce_color} ({strBold(f'+{ce_change}') if ce_change > 0 else strBold(ce_change) if ce_change < 0 else ce_change}), Put OI: {pe_color} ({strBold(f'+{pe_change}') if pe_change > 0 else strBold(pe_change) if pe_change < 0 else pe_change})")
+
+def calculate_support_resistance(oi_data):
+    highest_oi_ce = max(oi_data, key=lambda x: x[1])
+    highest_oi_pe = max(oi_data, key=lambda x: x[2])
+    return highest_oi_ce[0], highest_oi_pe[0]
+
+async def update_data():
+    global cookies
+    prev_bank_nifty_data = None
+    while True:
+        cookies = set_cookie()
+        indices_data, bnf_data, nf_data = await fetch_all_data()
+
+        process_indices_data(indices_data)
+
+        bank_nifty_oi_data = process_oi_data(bnf_data, bnf_nearest, 100, 10)
+        support_bank_nifty, resistance_bank_nifty = calculate_support_resistance(bank_nifty_oi_data)
+
+        print(strBold(strCyan(f"\nMajor Support and Resistance Levels:")))        
+        print(f"Bank Nifty Support: {strYellow(support_bank_nifty)}, Bank Nifty Resistance: {strYellow(resistance_bank_nifty)}")
+
+        print_oi_data("Bank Nifty", bank_nifty_oi_data, prev_bank_nifty_data)
+
+        prev_bank_nifty_data = bank_nifty_oi_data
+
+        for i in range(30, 0, -1):
+            print(strBold(strLightGray(f"\rFetching data in {i} seconds...")), end="")
+            time.sleep(1)
+        print(strBold(strCyan("\nFetching new data... Please wait.")))
+        await asyncio.sleep(1)
+    
+async def main():
+    await update_data()
+
+asyncio.run(main())
